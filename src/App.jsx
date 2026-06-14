dbbc6e12747b21aef47e08d2b2d1fa79b811ee65
joshrb46 <joshrb46@gmail.com>
2026-06-14 14:03:12 -0700

    All routes inside a shared layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { Routes, Route } from "react-router";
+
+import Layout from "../layout/Layout";
+import BooksPage from "../books/BooksPage";
+import BookDetails from "../books/BookDetails";
+import Register from "../auth/Register";
+import Login from "../auth/Login";
+import AccountPage from "../account/AccountPage";
+import Error404 from "./Error404.jsx";
+
 export default function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<BooksPage />} />
+        <Route path="/books" element={<BooksPage />} />
+        <Route path="/books/:id" element={<BookDetails />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Error404 />} />
+      </Route>
+    </Routes>
+  );
 }
