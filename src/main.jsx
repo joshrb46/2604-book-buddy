e9d82bc9ad3970f564fe8a60d614ee829077fa2b
joshrb46 <joshrb46@gmail.com>
2026-06-14 13:35:59 -0700

    App entry, wraps App in BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router";
+import { AuthProvider } from "../auth/AuthContext";
+import "../styles/index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>,
 );
